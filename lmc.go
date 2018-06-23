6c1eb93d12a5c331ca93a43dda16754f9e1f8978
Jamie Mansfield <dev@jamierocks.uk>
2018-06-24 00:02:36 +0100

    Reduce duplication in CLI

--- lmc.go
+++ lmc.go
@@ -7,10 +7,60 @@ import (
 	"net/url"
 	"fmt"
 	"github.com/jamiemansfield/lmcemu/asm"
+	"github.com/mileusna/conditional"
 	"encoding/json"
 	"github.com/jamiemansfield/lmcemu/emu"
-	"github.com/mileusna/conditional"
 )
+
+var (
+	inputFlag = cli.StringFlag{
+		Name: "input, i",
+		Usage: "the input file or URI",
+	}
+)
+
+type lmcFunc func(instructions []*asm.Instruction) error
+
+func createAction(lmcFunc lmcFunc) cli.ActionFunc {
+	return func(c *cli.Context) error {
+		input := c.String("input")
+		uri, err := url.Parse(input)
+		if err != nil {
+			return err
+		}
+
+		var instructions []*asm.Instruction
+		if uri.Scheme == "file" || uri.Scheme == "" {
+			file, err := os.Open(conditional.String(uri.Scheme == "", uri.Path, uri.Opaque))
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+
+			// Create a Parser and parse the file
+			parser := asm.CreateParser()
+			parser.ReadFromFile(file)
+
+			// Assemble the program.
+			insts, err := parser.Assemble()
+			if err != nil {
+				return err
+			}
+			instructions = insts
+		} else
+		if uri.Scheme == "builtin" {
+			insts := BuiltinRegistry[uri.Opaque]
+			if insts == nil {
+				return fmt.Errorf("compile: a builtin program of name '%s' does not exist", uri.Opaque)
+			}
+			instructions = insts
+		} else {
+			return fmt.Errorf("compile: unsupported URI scheme ('%s') used", uri.Scheme)
+		}
+
+		return lmcFunc(instructions)
+	}
+}
 
 func main() {
 	app := cli.NewApp()
@@ -24,47 +74,9 @@ func main() {
 			Aliases: []string{"c"},
 			Usage:   "compiles the given assembly file to machine code",
 			Flags:   []cli.Flag {
-				cli.StringFlag{
-					Name: "input, i",
-					Usage: "the input file or URI",
-				},
+				inputFlag,
 			},
-			Action:  func(c *cli.Context) error {
-				input := c.String("input")
-				uri, err := url.Parse(input)
-				if err != nil {
-					return err
-				}
-
-				var instructions []*asm.Instruction
-				if uri.Scheme == "file" || uri.Scheme == "" {
-					file, err := os.Open(conditional.String(uri.Scheme == "", uri.Path, uri.Opaque))
-					if err != nil {
-						return err
-					}
-					defer file.Close()
-
-					// Create a Parser and parse the file
-					parser := asm.CreateParser()
-					parser.ReadFromFile(file)
-
-					// Assemble the program.
-					insts, err := parser.Assemble()
-					if err != nil {
-						return err
-					}
-					instructions = insts
-				} else
-				if uri.Scheme == "builtin" {
-					insts := BuiltinRegistry[uri.Opaque]
-					if insts == nil {
-						return fmt.Errorf("compile: a builtin program of name '%s' does not exist", uri.Opaque)
-					}
-					instructions = insts
-				} else {
-					return fmt.Errorf("compile: unsupported URI scheme ('%s') used", uri.Scheme)
-				}
-
+			Action:  createAction(func (instructions []*asm.Instruction) error {
 				// Assemble the program into machine code
 				prog, err := asm.AssembleProgram(instructions)
 				if err != nil {
@@ -75,54 +87,16 @@ func main() {
 				fmt.Printf("%v", prog)
 
 				return nil
-			},
+			}),
 		},
 		{
 			Name:    "dump",
 			Aliases: []string{"d"},
 			Usage:   "dumps the given assembly file as a JSON output",
 			Flags:   []cli.Flag {
-				cli.StringFlag{
-					Name: "input, i",
-					Usage: "the input file or URI",
-				},
+				inputFlag,
 			},
-			Action:  func(c *cli.Context) error {
-				input := c.String("input")
-				uri, err := url.Parse(input)
-				if err != nil {
-					return err
-				}
-
-				var instructions []*asm.Instruction
-				if uri.Scheme == "file" || uri.Scheme == "" {
-					file, err := os.Open(conditional.String(uri.Scheme == "", uri.Path, uri.Opaque))
-					if err != nil {
-						return err
-					}
-					defer file.Close()
-
-					// Create a Parser and parse the file
-					parser := asm.CreateParser()
-					parser.ReadFromFile(file)
-
-					// Assemble the program.
-					insts, err := parser.Assemble()
-					if err != nil {
-						return err
-					}
-					instructions = insts
-				} else
-				if uri.Scheme == "builtin" {
-					insts := BuiltinRegistry[uri.Opaque]
-					if insts == nil {
-						return fmt.Errorf("compile: a builtin program of name '%s' does not exist", uri.Opaque)
-					}
-					instructions = insts
-				} else {
-					return fmt.Errorf("compile: unsupported URI scheme ('%s') used", uri.Scheme)
-				}
-
+			Action:  createAction(func (instructions []*asm.Instruction) error {
 				// Dump in JSON form
 				bytes, err := json.MarshalIndent(instructions, "", "\t")
 				if err != nil {
@@ -131,54 +105,16 @@ func main() {
 				fmt.Println(string(bytes))
 
 				return nil
-			},
+			}),
 		},
 		{
 			Name:    "run",
 			Aliases: []string{"r"},
 			Usage:   "runs the given assembly file",
 			Flags:   []cli.Flag {
-				cli.StringFlag{
-					Name: "input, i",
-					Usage: "the input file or URI",
-				},
+				inputFlag,
 			},
-			Action:  func(c *cli.Context) error {
-				input := c.String("input")
-				uri, err := url.Parse(input)
-				if err != nil {
-					return err
-				}
-
-				var instructions []*asm.Instruction
-				if uri.Scheme == "file" || uri.Scheme == "" {
-					file, err := os.Open(conditional.String(uri.Scheme == "", uri.Path, uri.Opaque))
-					if err != nil {
-						return err
-					}
-					defer file.Close()
-
-					// Create a Parser and parse the file
-					parser := asm.CreateParser()
-					parser.ReadFromFile(file)
-
-					// Assemble the program.
-					insts, err := parser.Assemble()
-					if err != nil {
-						return err
-					}
-					instructions = insts
-				} else
-				if uri.Scheme == "builtin" {
-					insts := BuiltinRegistry[uri.Opaque]
-					if insts == nil {
-						return fmt.Errorf("compile: a builtin program of name '%s' does not exist", uri.Opaque)
-					}
-					instructions = insts
-				} else {
-					return fmt.Errorf("compile: unsupported URI scheme ('%s') used", uri.Scheme)
-				}
-
+			Action:  createAction(func (instructions []*asm.Instruction) error {
 				// Assemble the program into machine code
 				prog, err := asm.AssembleProgram(instructions)
 				if err != nil {
@@ -194,7 +130,7 @@ func main() {
 				}
 
 				return nil
-			},
+			}),
 		},
 	}
 
